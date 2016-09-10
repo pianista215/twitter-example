24d33f0dec878db1f8a987369a5c36fb3940a305
Unai Sarasola <usarasola@stratio.com>
2016-09-10 19:23:16 +0200

    Reading terms from config

--- src/main/scala/com/devsmobile/twitter_example/TwitterReaderLauncher.scala
+++ src/main/scala/com/devsmobile/twitter_example/TwitterReaderLauncher.scala
@@ -2,6 +2,8 @@ package com.devsmobile.twitter_example
 
 import akka.actor.{ActorSystem, Props}
 import com.devsmobile.twitter_example.actors.TwitterReader
+import com.devsmobile.twitter_example.common.TwitterExConfig
+import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
 /**
@@ -12,7 +14,17 @@ object TwitterReaderLauncher extends App with LazyLogging {
   logger.info("Starting Actor System and Actors")
   val system = ActorSystem("MyActorSystem")
 
-  val twitterReader = system.actorOf(Props[TwitterReader], name = "initialActor")
-  twitterReader ! TwitterReader.Start(List("Barcelona"))
+  val team = TwitterExConfig.config.getString("listener.team")
+  logger.info(s"Team: $team")
+
+  val teamTermsConfig = ConfigFactory.load(s"teams/$team.conf")
+  if(!teamTermsConfig.hasPath("general.name")){
+    logger.error(s"Not found config for team: $team")
+    system.terminate()
+  } else {
+    val twitterReader = system.actorOf(Props[TwitterReader], name = "initialActor")
+    twitterReader ! TwitterReader.Start(teamTermsConfig withFallback(TwitterExConfig.config))
+  }
+
 
 }
--- src/main/scala/com/devsmobile/twitter_example/actors/TwitterReader.scala
+++ src/main/scala/com/devsmobile/twitter_example/actors/TwitterReader.scala
@@ -3,7 +3,10 @@ package com.devsmobile.twitter_example.actors
 import akka.actor.{Actor, Props}
 import com.devsmobile.twitter_example.actors.TwitterReader.Start
 import com.devsmobile.twitter_example.reader.HbcClient
+import com.typesafe.config.{Config, ConfigList, ConfigValue}
 import com.typesafe.scalalogging.LazyLogging
+
+import collection.JavaConverters._
 
 /**
   * Created by pianista on 31/08/16.
@@ -11,15 +14,44 @@ import com.typesafe.scalalogging.LazyLogging
 class TwitterReader extends HbcClient with Actor with LazyLogging {
 
   override def receive: Receive = {
-    case Start(terms) =>
-      logger.info(s"Starting listening tweets for: ${terms mkString(",")}.")
-      val (client, queue) = startListeningFor(terms)
+    case Start(teamConfig) =>
+      val termsToListen = obtainTermsToListen(teamConfig)
+      logger.info(s"Starting listening tweets for: ${termsToListen mkString(",")}.")
+      val (client, queue) = startListeningFor(termsToListen)
       val queueConsumer = context.system.actorOf(Props[QueueConsumer], name = "queueConsumer")
       queueConsumer ! QueueConsumer.StartConsumingFrom(queue)
   }
 
+  private def obtainTermsToListen(config: Config): List[String] =
+    genericFootballTerms(config) ::: genericTeamTerms(config) ::: coach(config) ::: president(config) ::: players(config)
+
+  private def genericFootballTerms(config: Config): List[String] =
+    (config.getStringList("football.terms") asScala) toList
+
+  private def genericTeamTerms(config: Config): List[String] =
+    config.getString("general.name") :: ((config.getStringList("general.related") asScala) toList)
+
+  private def coach(config: Config): List[String] =
+    (config.getStringList("team.coach") asScala) toList
+
+  private def president(config: Config): List[String] =
+    (config.getStringList("team.president") asScala) toList
+
+  private def players(config: Config): List[String] = {
+    val playersConfig = config.getConfig("team.players")
+    val playersEntries = playersConfig.entrySet() asScala
+
+    val termsPerPlayer = playersEntries map {
+      case entry: java.util.Map.Entry[String, ConfigValue]  =>
+        playersConfig.getStringList(entry.getKey) asScala
+    } toList
+
+    termsPerPlayer.flatten
+  }
+
+
 }
 
 object TwitterReader {
-  case class Start(terms: List[String])
+  case class Start(teamTerms: Config)
 }
